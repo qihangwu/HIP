a77c736f044993efc66c70ae598d886c75ab1bf3
Max Zhang <myzhang01@berkeley.edu>
2021-05-29 13:00:29 -0700

    cleaning and figures for IB

--- code/jobs_in_HIP.R
+++ code/jobs_in_HIP.R
@@ -57,3 +57,101 @@ ggplot(data = hip_IA) +
   ggsave('figures/jobs_in_hip/IA5s.png')
 
 
+#### IB ####
+
+IB <- c('guess_hip_hour',
+        'guess_hip_hour_sure',
+        'guess_hip_day',
+        'guess_hip_day_sure',
+        'guess_hip_extra',
+        'guess_hip_extra_sure',
+        'guess_hip_night',
+        'guess_hip_night_sure',
+        'guess_hip_transp',
+        'guess_hip_transp_sure',
+        'guess_hip_lunch',
+        'guess_hip_lunch_sure',
+        'guess_hip_attend',
+        'guess_hip_attend_sure')
+
+hip_IB <- hip %>%
+  select(all_of(IB)) %>%
+  mutate(across(IB[seq(1, 13, 2)], as.integer)) %>%
+  mutate(across(IB[seq(2, 14, 2)], as.factor)) %>%
+  mutate(across(IB[seq(2, 14, 2)], ~recode(.,
+                                           `0` = 'very sure',
+                                           `1` = 'slightly sure',
+                                           `2` = 'slightly not sure',
+                                           `3` = 'not sure at all')))
+
+ggplot(data = hip_IB) +
+  geom_histogram(aes(x = guess_hip_hour), binwidth = 1) +
+  ggsave('figures/jobs_in_HIP/IB1.png')
+
+ggplot(data = hip_IB) +
+  geom_bar(aes(x = guess_hip_hour_sure)) +
+  ggsave('figures/jobs_in_HIP/IB1s.png')
+
+ggplot(data = hip_IB) +
+  geom_histogram(aes(x = guess_hip_day), binwidth = 1) +
+  ggsave('figures/jobs_in_HIP/IB2.png')
+
+ggplot(data = hip_IB) +
+  geom_bar(aes(x = guess_hip_day_sure)) +
+  ggsave('figures/jobs_in_HIP/IB2s.png')
+
+ggplot(data = hip_IB) +
+  geom_histogram(aes(x = guess_hip_extra), binwidth = 1) +
+  ggsave('figures/jobs_in_HIP/IB3.png')
+
+ggplot(data = hip_IB) +
+  geom_bar(aes(x = guess_hip_extra_sure)) +
+  ggsave('figures/jobs_in_HIP/IB3s.png')
+
+ggplot(data = hip_IB) +
+  geom_histogram(aes(x = guess_hip_night), binwidth = 1) +
+  ggsave('figures/jobs_in_HIP/IB4.png')
+
+ggplot(data = hip_IB) +
+  geom_bar(aes(x = guess_hip_night_sure)) +
+  ggsave('figures/jobs_in_HIP/IB4s.png')
+
+ggplot(data = hip_IB) +
+  geom_histogram(aes(x = guess_hip_transp), binwidth = 1) +
+  ggsave('figures/jobs_in_HIP/IB5.png')
+
+ggplot(data = hip_IB) +
+  geom_bar(aes(x = guess_hip_transp_sure)) +
+  ggsave('figures/jobs_in_HIP/IB5s.png')
+
+ggplot(data = hip_IB) +
+  geom_histogram(aes(x = guess_hip_lunch), binwidth = 1) +
+  ggsave('figures/jobs_in_HIP/IB6.png')
+
+ggplot(data = hip_IB) +
+  geom_bar(aes(x = guess_hip_lunch_sure)) +
+  ggsave('figures/jobs_in_HIP/IB6s.png')
+
+ggplot(data = hip_IB) +
+  geom_histogram(aes(x = guess_hip_attend), binwidth = 1) +
+  ggsave('figures/jobs_in_HIP/IB7.png')
+
+ggplot(data = hip_IB) +
+  geom_bar(aes(x = guess_hip_attend_sure)) +
+  ggsave('figures/jobs_in_HIP/IB7s.png')
+
+
+#### IC ####
+
+
+
+
+#### ID ####
+
+
+
+
+
+
+
+
