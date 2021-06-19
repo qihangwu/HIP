1c3231aeaed8d019bf1a2b54ebc1112b4590b03f
ezanaanley <85189815+ezanaanley@users.noreply.github.com>
2021-06-19 08:16:22 -0400

    Update Type (AT, WM - morning).R

--- code/Type (AT, WM - morning).R
+++ code/Type (AT, WM - morning).R
@@ -1,367 +1,237 @@
 library(tidyverse)
 library(readxl)
 library(ggplot2)
-setwd("C:/Users/Ezana Teodros/Downloads")
-
+setwd("~/HIP")
 
 data = read_excel('weekdayend_all.xlsx') 
 
-#### A11-A36 (Grading - Behavioral Test (BT)) ####
+#### AT (Evening) ####
 
-# Question regarding 1 is correct, zero is wrong
+# Question regarding AT11 AT12language variable(s)
 
-BT = c('a11',
-       'a12',
-       'a13',
-       'a14',
-       'a15',
-       'a16',
-       'a21',
-       'a22',
-       'a23',
-       'a24',
-       'a25',
-       'a26',
-       'a31',
-       'a32',
-       'a33',
-       'a34',
-       'a35',
-       'a36')
+AT = c('tipi1',
+       'tipi2',
+       'tipi3',
+       'tipi4',
+       'tipi5',
+       'tipi6',
+       'tipi7',
+       'tipi8',
+       'tipi9',
+       'tipi10')
 
- 
-data_BT = data %>%
-  select(all_of(BT)) %>%
-  mutate(across(BT[c(1:18)], as.factor)) %>%
-  mutate(a11 = recode(a11,
-                           `0` = '100 birrs today.',
-                           `1` = 'Lottery today, 80% chance of 125 birrs, 20% of getting nothing.',
-                           `-9` = 'Missing')) %>%
-  mutate(a12 = recode(a12,
-                      `0` = '100 birrs today.',
-                      `1` = 'Lottery today, 70% chance of 143 birrs, 30% of getting nothing.',
-                      `-9` = 'Missing')) %>%
-  mutate(a13 = recode(a13,
-                      `0` = '100 birrs today.',
-                      `1` = 'Lottery today, 60% chance of 167 birrs, 40% of getting nothing.',
-                      `-9` = 'Missing')) %>%
-  mutate(a14 = recode(a14,
-                      `0` = '100 birrs today.',
-                      `1` = 'Lottery today, 50% chance of 200 birrs, 50% of getting nothing.',
-                      `-9` = 'Missing')) %>%
-  mutate(a15 = recode(a15,
-                      `0` = '100 birrs today.',
-                      `1` = 'Lottery today, 40% chance of 250 birrs, 60% of getting nothing.',
-                      `-9` = 'Missing')) %>%
-  mutate(a16 = recode(a16,
-                      `0` = '100 birrs today.',
-                      `1` = 'Lottery today, 30% chance of 333 birrs, 70% of getting nothing.',
-                      `-9` = 'Missing')) %>%
-  mutate(a21 = recode(a21,
-                      `0` = '100 birrs today.',
-                      `1` = '105 birrs in a year.',
-                      `-9` = 'Missing')) %>%
-  mutate(a22 = recode(a22,
-                      `0` = '100 birrs today.',
-                      `1` = '110 birrs in a year.',
-                      `-9` = 'Missing')) %>%
-  mutate(a23 = recode(a23,
-                      `0` = '100 birrs today.',
-                      `1` = '120 birrs in a year.',
-                      `-9` = 'Missing')) %>%
-  mutate(a24 = recode(a24,
-                      `0` = '100 birrs today.',
-                      `1` = '130 birrs in a year.',
-                      `-9` = 'Missing')) %>%  
-  mutate(a25 = recode(a25,
-                      `0` = '100 birrs today.',
-                      `1` = '140 birrs in a year.',
-                      `-9` = 'Missing')) %>%
-  mutate(a26 = recode(a26,
-                      `0` = '100 birrs today.',
-                      `1` = '150 birrs in a year.',
-                      `-9` = 'Missing')) %>%
-  mutate(a31 = recode(a31,
-                      `0` = 'Get 100 birrs in 6 months.',
-                      `1` = 'Get 105 birrs in a year.',
-                      `-9` = 'Missing')) %>%
-  mutate(a32 = recode(a32,
-                      `0` = 'Get 100 birrs in 6 months.',
-                      `1` = 'Get 110 birrs in a year.',
-                      `-9` = 'Missing')) %>%
-  mutate(a33 = recode(a33,
-                      `0` = 'Get 100 birrs in 6 months.',
-                      `1` = 'Get 120 birrs in a year.',
-                      `-9` = 'Missing')) %>%  
-  mutate(a34 = recode(a34,
-                      `0` = 'Get 100 birrs in 6 months.',
-                      `1` = 'Get 130 birrs in a year.',
-                      `-9` = 'Missing')) %>%
-  mutate(a35 = recode(a35,
-                    `0` = 'Get 100 birrs in 6 months.',
-                    `1` = 'Get 140 birrs in a year.',
-                    `-9` = 'Missing')) %>%
-  mutate(a36 = recode(a36,
-                      `0` = 'Get 100 birrs in 6 months.',
-                      `1` = 'Get 150 birrs in a year.',
-                      `-9` = 'Missing'))
+data_AT = data %>%
+  select(all_of(AT)) %>%
+  mutate(across(AT[c(1:10)], as.factor)) %>%
+  mutate(across(AT[c(1:10)], ~recode(.,
+                                     `0` = 'Completely Agree',
+                                     `1` = 'Somewhat Agree',
+                                     `2` = 'Neither agree nor disagree',
+                                     `3` = 'Somewhat Disagree',
+                                     `4` = 'Completely Disagree'))) 
 
-str(BT)
+str(data_AT) 
+
+ggplot(data = data_AT) +
+  geom_bar(aes(x = tipi1)) +
+  ggsave('AT1.png')
+
+ggplot(data = data_AT) +
+  geom_bar(aes(x = tipi2)) +
+  ggsave('AT2.png')
+
+ggplot(data = data_AT) +
+  geom_bar(aes(x = tipi3)) +
+  ggsave('AT3.png')
+
+ggplot(data = data_AT) +
+  geom_bar(aes(x = tipi4)) +
+  ggsave('AT4.png')
+
+ggplot(data = data_AT) +
+  geom_bar(aes(x = tipi5)) +
+  ggsave('AT5.png')
+
+ggplot(data = data_AT) +
+  geom_bar(aes(x = tipi6)) +
+  ggsave('AT6.png')
+
+ggplot(data = data_AT) +
+  geom_bar(aes(x = tipi7)) +
+  ggsave('AT7.png')
+
+ggplot(data = data_AT) +
+  geom_bar(aes(x = tipi8)) +
+  ggsave('AT8.png')
+
+ggplot(data = data_AT) +
+  geom_bar(aes(x = tipi9)) +
+  ggsave('AT9.png')
+
+ggplot(data = data_AT) +
+  geom_bar(aes(x = tipi10)) +
+  ggsave('AT10.png')
 
 
-ggplot(data = data_BT) +
-  geom_bar(aes(x = a11)) +
-  ggsave('a11.png')
+#### WM (Evening) ####
 
-ggplot(data = data_BT) +
-  geom_bar(aes(x = a12)) +
-  ggsave('a12.png')
+data$tot_score_WM1 = data$series11_yesno + data$series12_yesno + data$series13_yesno + data$series14_yesno + data$series15_yesno + 
+  data$series16_yesno + data$series17_yesno
 
-ggplot(data = data_BT) +
-  geom_bar(aes(x = a13)) +
-  ggsave('a13.png')
+WM1 = c('series11_yesno',
+        'series12_yesno',
+        'series13_yesno',
+        'series14_yesno',
+        'series15_yesno',
+        'series16_yesno',
+        'series17_yesno',
+        'tot_score_WM1')
 
-ggplot(data = data_BT) +
-  geom_bar(aes(x = a14)) +
-  ggsave('a14.png')
+data_WM1 = data %>%
+  select(all_of(WM1)) %>%
+  mutate(across(WM1[c(1:7)], as.factor)) %>%
+  mutate(across(WM1[c(8)], as.integer)) %>%
+  mutate(across(WM1[c(1:7)], ~recode(.,
+                                     `0` = 'Incorrect',
+                                     `1` = 'Correct'))) 
 
-ggplot(data = data_BT) +
-  geom_bar(aes(x = a15)) +
-  ggsave('a15.png')
- 
-ggplot(data = data_BT) +
-  geom_bar(aes(x = a16)) +
-  ggsave('a16.png')
+min(data$tot_score_WM1, na.rm=T)
+max(data$tot_score_WM1, na.rm=T)
 
-ggplot(data = data_BT) +
-  geom_bar(aes(x = a21)) +
-  ggsave('a21.png')
-
-ggplot(data = data_BT) +
-  geom_bar(aes(x = a22)) +
-  ggsave('a22.png')
-
-ggplot(data = data_BT) +
-  geom_bar(aes(x = a23)) +
-  ggsave('a23.png')
-
-ggplot(data = data_BT) +
-  geom_bar(aes(x = a24)) +
-  ggsave('a24.png')
-
-ggplot(data = data_BT) +
-  geom_bar(aes(x = a25)) +
-  ggsave('a25.png')
-
-ggplot(data = data_BT) +
-  geom_bar(aes(x = a26)) +
-  ggsave('a26.png')
-
-ggplot(data = data_BT) +
-  geom_bar(aes(x = a31)) +
-  ggsave('a31.png')
-
-ggplot(data = data_BT) +
-  geom_bar(aes(x = a32)) +
-  ggsave('a32.png')
-
-ggplot(data = data_BT) +
-  geom_bar(aes(x = a33)) +
-  ggsave('a33.png')
-
-ggplot(data = data_BT) +
-  geom_bar(aes(x = a34)) +
-  ggsave('a34.png')
-
-ggplot(data = data_BT) +
-  geom_bar(aes(x = a35)) +
-  ggsave('a35.png')
-
-ggplot(data = data_BT) +
-  geom_bar(aes(x = a36)) +
-  ggsave('a36.png')
-
-#### B1-B12 (Grading - Cognitive Test (CT)) ####
-
-# Question regarding values to put for 1-6
-
-CT = c('b1',
-       'b2',
-       'b3',
-       'b4',
-       'b5',
-       'b6',
-       'b7',
-       'b8',
-       'b9',
-       'b10',
-       'b11',
-       'b12')
-
-data_CT = data %>%
-  select(all_of(CT)) %>%
-  mutate(across(CT[c(1:12)], as.factor)) %>%
-  mutate(across(CT[c(1)], ~recode(.,
-                                     `1` = 'Incorrect',
-                                     `2` = 'Correct',
-                                     `3` = 'Incorrect',
-                                     `4` = 'Incorrect',
-                                     `5` = 'Incorrect',
-                                     `6` = 'Incorrect'))) %>%
-  mutate(across(CT[c(2)], ~recode(.,
-                                `1` = 'Incorrect',
-                                `2` = 'Incorrect',
-                                `3` = 'Incorrect',
-                                `4` = 'Incorrect',
-                                `5` = 'Incorrect',
-                                `6` = 'Correct'))) %>%
-  mutate(across(CT[c(3)], ~recode(.,
-                                  `1` = 'Correct',
-                                  `2` = 'Incorrect',
-                                  `3` = 'Incorrect',
-                                  `4` = 'Incorrect',
-                                  `5` = 'Incorrect',
-                                  `6` = 'Incorrect'))) %>%
-  mutate(across(CT[c(4)], ~recode(.,
-                                  `1` = 'Incorrect',
-                                  `2` = 'Correct',
-                                  `3` = 'Incorrect',
-                                  `4` = 'Incorrect',
-                                  `5` = 'Incorrect',
-                                  `6` = 'Incorrect'))) %>%
-  mutate(across(CT[c(5)], ~recode(.,
-                                  `1` = 'Correct',
-                                  `2` = 'Incorrect',
-                                  `3` = 'Incorrect',
-                                  `4` = 'Incorrect',
-                                  `5` = 'Incorrect',
-                                  `6` = 'Incorrect'))) %>%
-  mutate(across(CT[c(6)], ~recode(.,
-                                  `1` = 'Incorrect',
-                                  `2` = 'Incorrect',
-                                  `3` = 'Correct',
-                                  `4` = 'Incorrect',
-                                  `5` = 'Incorrect',
-                                  `6` = 'Incorrect'))) %>% 
-  mutate(across(CT[c(7)], ~recode(.,
-                                  `1` = 'Incorrect',
-                                  `2` = 'Incorrect',
-                                  `3` = 'Incorrect',
-                                  `4` = 'Incorrect',
-                                  `5` = 'Correct',
-                                  `6` = 'Incorrect'))) %>% 
-  mutate(across(CT[c(8)], ~recode(.,
-                                  `1` = 'Incorrect',
-                                  `2` = 'Incorrect',
-                                  `3` = 'Incorrect',
-                                  `4` = 'Incorrect',
-                                  `5` = 'Incorrect',
-                                  `6` = 'Correct'))) %>%
-  mutate(across(CT[c(9)], ~recode(.,
-                                  `1` = 'Incorrect',
-                                  `2` = 'Incorrect',
-                                  `3` = 'Incorrect',
-                                  `4` = 'Correct',
-                                  `5` = 'Incorrect',
-                                  `6` = 'Incorrect'))) %>%
-  mutate(across(CT[c(10)], ~recode(.,
-                                  `1` = 'Incorrect',
-                                  `2` = 'Incorrect',
-                                  `3` = 'Correct',
-                                  `4` = 'Incorrect',
-                                  `5` = 'Incorrect',
-                                  `6` = 'Incorrect'))) %>%
-  mutate(across(CT[c(11)], ~recode(.,
-                                   `1` = 'Incorrect',
-                                   `2` = 'Incorrect',
-                                   `3` = 'Correct',
-                                   `4` = 'Incorrect',
-                                   `5` = 'Incorrect',
-                                   `6` = 'Incorrect'))) %>%
-  mutate(across(CT[c(12)], ~recode(.,
-                                   `1` = 'Incorrect',
-                                   `2` = 'Incorrect',
-                                   `3` = 'Incorrect',
-                                   `4` = 'Incorrect',
-                                   `5` = 'Correct',
-                                   `6` = 'Incorrect'))) 
-str(data_CT) 
-
-ggplot(data = data_CT) +
-  geom_bar(aes(x = b1)) +
-  ggsave('b1.png')
-
-ggplot(data = data_CT) +
-  geom_bar(aes(x = b2)) +
-  ggsave('b2.png')
-
-ggplot(data = data_CT) +
-  geom_bar(aes(x = b3)) +
-  ggsave('b3.png')
-
-ggplot(data = data_CT) +
-  geom_bar(aes(x = b4)) +
-  ggsave('b4.png')
-
-ggplot(data = data_CT) +
-  geom_bar(aes(x = b5)) +
-  ggsave('b5.png')
-
-ggplot(data = data_CT) +
-  geom_bar(aes(x = b6)) +
-  ggsave('b6.png')
-
-ggplot(data = data_CT) +
-  geom_bar(aes(x = b7)) +
-  ggsave('b7.png')
-
-ggplot(data = data_CT) +
-  geom_bar(aes(x = b8)) +
-  ggsave('b8.png')
-
-ggplot(data = data_CT) +
-  geom_bar(aes(x = b9)) +
-  ggsave('b9.png')
-
-ggplot(data = data_CT) +
-  geom_bar(aes(x = b10)) +
-  ggsave('b10.png')
-
-ggplot(data = data_CT) +
-  geom_bar(aes(x = b11)) +
-  ggsave('b11.png')
-
-ggplot(data = data_CT) +
-  geom_bar(aes(x = b12)) +
-  ggsave('b12.png')
+str(data_WM1) 
 
 
-#### B1-B12 (Grading - Dexterity (DX)) ####
+ggplot(data = data_WM1) +
+  geom_bar(aes(x = series11_yesno)) +
+  ggsave('WM11.png')
 
-DX = c('card_qual',
-       'needle_qual')
+ggplot(data = data_WM1) +
+  geom_bar(aes(x = series12_yesno)) +
+  ggsave('WM2.png')
 
-data_DX = data %>%
-  select(all_of(DX)) %>%
-  mutate(across(DX[c(1:2)], as.factor)) %>%
-  mutate(card_qual= recode(card_qual,
-                                     `0` = 'Two piles have the same number of cards.',
-                                     `1` = 'Two piles have similar number of cards but not exactly the same.',
-                                     `2` = 'Two piles have very differnt number of cards.',
-                                     `-9` = 'Missing')) %>%
-  
-  mutate(needle_qual= recode(needle_qual,
-                              `0` = 'Worker pulled through 2 or 3 needles.',
-                              `1` = 'Worker pulled 1 needle.',
-                              `2` = 'Worker pulled 0 needle.',
-                              `-9` = 'Missing')) 
+ggplot(data = data_WM1) +
+  geom_bar(aes(x = series13_yesno)) +
+  ggsave('WM3.png')
 
-str(data_CT) 
+ggplot(data = data_WM1) +
+  geom_bar(aes(x = series14_yesno)) +
+  ggsave('WM4.png')
 
-ggplot(data = data_DX) +
-  geom_bar(aes(x = card_qual)) +
-  ggsave('DX1.png')
+ggplot(data = data_WM1) +
+  geom_bar(aes(x = series15_yesno)) +
+  ggsave('WM5.png')
 
-ggplot(data = data_DX) +
-  geom_bar(aes(x = needle_qual)) +
-  ggsave('DX2.png')
+ggplot(data = data_WM1) +
+  geom_bar(aes(x = series16_yesno)) +
+  ggsave('WM6.png')
+
+ggplot(data = data_WM1) +
+  geom_bar(aes(x = series17_yesno)) +
+  ggsave('WM7.png')
+
+data$tot_score_WM2 = data$series21_yesno + data$series22_yesno + data$series23_yesno + data$series24_yesno + data$series25_yesno + 
+  data$series26_yesno + data$series27_yesno
+
+WM2 = c('series21_yesno',
+        'series22_yesno',
+        'series23_yesno',
+        'series24_yesno',
+        'series25_yesno',
+        'series26_yesno',
+        'series27_yesno',
+        'tot_score_WM2')
+
+data_WM2 = data %>%
+  select(all_of(WM2)) %>%
+  mutate(across(WM2[c(1:7)], as.factor)) %>%
+  mutate(across(WM2[c(1:7)], ~recode(.,
+                                     `0` = 'Incorrect',
+                                     `1` = 'Correct'))) 
+
+min(data$tot_score_WM2, na.rm=T)
+max(data$tot_score_WM2, na.rm=T)
+
+ggplot(data = data_WM2) +
+  geom_bar(aes(x = series21_yesno)) +
+  ggsave('WM21.png')
+
+ggplot(data = data_WM2) +
+  geom_bar(aes(x = series22_yesno)) +
+  ggsave('WM22.png')
+
+ggplot(data = data_WM2) +
+  geom_bar(aes(x = series23_yesno)) +
+  ggsave('WM23.png')
+
+ggplot(data = data_WM2) +
+  geom_bar(aes(x = series24_yesno)) +
+  ggsave('WM24.png')
+
+ggplot(data = data_WM2) +
+  geom_bar(aes(x = series25_yesno)) +
+  ggsave('WM25.png')
+
+ggplot(data = data_WM2) +
+  geom_bar(aes(x = series26_yesno)) +
+  ggsave('WM26.png')
+
+ggplot(data = data_WM2) +
+  geom_bar(aes(x = series27_yesno)) +
+  ggsave('WM27.png')
+
+
+data$tot_score_WM3 = data$series31_yesno + data$series32_yesno + data$series33_yesno + data$series34_yesno + data$series35_yesno + 
+  data$series36_yesno + data$series37_yesno
+
+WM3 = c('series31_yesno',
+        'series32_yesno',
+        'series33_yesno',
+        'series34_yesno',
+        'series35_yesno',
+        'series36_yesno',
+        'series37_yesno',
+        'tot_score_WM3')
+data_WM3__norm = function(x) {
+  return((x - min(x, na.rm = TRUE)) / (max(x, na.rm = TRUE) - min(x, na.rm = TRUE)))
+}
+
+norm_WM3 = as.data.frame(apply(data[1:7], 2, data_WM3__norm))
+
+
+data_WM3 = data %>%
+  select(all_of(WM3)) %>%
+  mutate(across(WM3[c(1:7)], as.factor)) %>%
+  mutate(across(WM3[c(1:7)], ~recode(.,
+                                     `0` = 'Incorrect',
+                                     `1` = 'Correct'))) 
+
+min(data$tot_score_WM3, na.rm=T)
+max(data$tot_score_WM3, na.rm=T)
+
+ggplot(data = data_WM3) +
+  geom_bar(aes(x = series31_yesno)) +
+  ggsave('WM31.png')
+
+ggplot(data = data_WM3) +
+  geom_bar(aes(x = series32_yesno)) +
+  ggsave('WM32.png')
+
+ggplot(data = data_WM3) +
+  geom_bar(aes(x = series33_yesno)) +
+  ggsave('WM33.png')
+
+ggplot(data = data_WM3) +
+  geom_bar(aes(x = series34_yesno)) +
+  ggsave('WM34.png')
+
+ggplot(data = data_WM3) +
+  geom_bar(aes(x = series35_yesno)) +
+  ggsave('WM35.png')
+
+ggplot(data = data_WM3) +
+  geom_bar(aes(x = series36_yesno)) +
+  ggsave('WM36.png')
+
+ggplot(data = data_WM3) +
+  geom_bar(aes(x = series37_yesno)) +
+  ggsave('WM37.png')
