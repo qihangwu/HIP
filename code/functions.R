93077ec441d113596d78d1cada4c909e75d1f110
Max Zhang <myzhang01@berkeley.edu>
2021-06-21 22:49:58 -0700

    create function calc_bias

---
+++ code/functions.R
@@ -0,0 +1,19 @@
+
+# Generate bias variables -------------------------------------------------
+
+calc_bias <- function(data, variable, benchmark) {
+  data <- data %>%
+    mutate('{variable}_raw' := !!sym(variable) - benchmark,   # raw bias
+           .after = !!sym(variable)) %>%
+
+    mutate('{variable}_abs' := abs(!!sym(variable)) / benchmark,   # absolute bias
+           .after = !!sym(paste0(variable, '_raw'))) %>%
+
+    mutate('{variable}_bias' := ifelse(!!sym(paste0(variable, '_abs')) >= threshold, 1, 0),   # bias sign
+           .after = !!sym(paste0(variable, '_abs'))) %>%
+    mutate('{variable}_bias' := ifelse(!!sym(paste0(variable, '_bias')) &
+                                         sign(!!sym(paste0(variable, '_raw'))) == -1,
+                                       -1,
+                                       !!sym(paste0(variable, '_bias'))))
+}
+
