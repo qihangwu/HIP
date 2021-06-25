4d12f4f53dc7b6be0e73fbfbe4e3524c23b577a3
ezanaanley <85189815+ezanaanley@users.noreply.github.com>
2021-06-25 10:00:27 -0400

    Update merge.R

--- code/merge.R
+++ code/merge.R
@@ -4,6 +4,11 @@
 source('code/outside_options.R')
 source('code/jobs_in_HIP.R')
 source('code/jobs_in_HIP_followup.R')
+source('code/DM_JA_TR_M.R')
+source('code/DM_WF_JH_E.R')
+source('code/Type_AT_WM_M.R')
+source('code/Grading_BT_CT_DX.R')
+source('code/MFA.R')
 
 
 # Merge -------------------------------------------------------------------
@@ -16,4 +21,10 @@ source('code/jobs_in_HIP_followup.R')
 hip_analysis <- hip %>%
   select(c('wid', 'treat1', 'treat2a', 'treat2b')) %>%
   cbind(hip_IC) %>%   # select only the salary variables?
-  cbind(hip_ID)
+  cbind(hip_ID) %>%
+  cbind(data_DMm) %>%
+  cbind(data_JH) %>%
+  cbind(data_TS_WM) %>%
+  cbind(data_CT) %>%
+  cbind(data_DX) %>%
+  cbind(data_MFA)
