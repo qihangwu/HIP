0ba99ac66633c119ada481f9a9ff7b7b8afb9da4
max zhang <myzhang01@berkeley.edu>
2021-06-30 13:50:11 -0700

    create function gg_sep()

--- code/merge.R
+++ code/merge.R
@@ -38,7 +38,8 @@ hip_analysis <- hip %>%
 
 hip_analysis_pool <- hip_analysis %>%
   mutate(treat2 = ifelse(treat2a == 1 | treat2b == 1, 1, 0),
-         .after = treat2b)
+         .after = treat2b) %>%
+  mutate(treat12 = treat1 * treat2)
 
 ## No interaction ---------------------------------------------------------
 
@@ -65,6 +66,25 @@ gg_pooled <- function(variable, benchmark) {
   return(plot)
 }
 
+gg_sep <- function(variable, benchmark) {
+
+  plot <- ggplot(data = hip_analysis_pool) +
+    geom_density(aes(x = !!sym(variable)),
+                 size = 0.8) +
+    geom_density(aes(x = !!sym(paste0('w_', variable))),
+                 size = 0.8,
+                 color = 'cornflowerblue') +
+    geom_vline(aes(xintercept = benchmark),
+               color = 'red') +
+    facet_wrap(~treat1 + treat2a + treat2b) +
+    labs(title = variable,
+         subtitle = 'Black is morning, blue is weekend',
+         x = '') +
+    theme_minimal()
+
+  return(plot)
+}
+
 ggplot() +
   geom_boxplot(data = hip_analysis_pool %>% filter(treat1 == 0), aes(y = guess_entry_salary)) +
   geom_boxplot(data = hip_analysis_pool %>% filter(treat1 == 1), aes(y = guess_entry_salary))
@@ -78,7 +98,7 @@ hip_analysis_pool <- hip_analysis_pool %>%
   mutate(guess_salary_medium_diff = w_guess_salary_medium - guess_salary_medium,
          .after = guess_salary_medium_bias)
 
-reg1 <- lm(guess_entry_salary_diff ~ treat1 + treat2, data = hip_analysis_pool)
+reg1 <- lm(w_guess_salary_medium_bias ~ treat1 + treat2 + treat12 + guess_salary_medium_bias, data = hip_analysis_pool)
 
 reg2 <- lm(guess_salary_medium_diff ~ treat1 + treat2, data = hip_analysis_pool)
 
