7fc2fbb387671773bc86f58b0c4fe8d25a9b99d1
Max Zhang <myzhang01@berkeley.edu>
2021-06-30 10:36:29 -0700

    create density curves

--- code/merge.R
+++ code/merge.R
@@ -1,6 +1,8 @@
 
 # Setup -------------------------------------------------------------------
 
+library(stargazer)
+
 source('code/outside_options.R')
 source('code/jobs_in_HIP.R')
 source('code/jobs_in_HIP_followup.R')
@@ -20,10 +22,10 @@ source('code/MFA.R')
 
 hip_analysis <- hip %>%
   select(c('wid', 'treat1', 'treat2a', 'treat2b')) %>%
-  cbind(hip_IC) %>%   # select only the salary variables?
-  cbind(hip_ID) %>%
-  cbind(hip_w_IC) %>%
-  cbind(hip_w_ID) %>%
+  cbind(hip_IC[c(1:4, 6:9, 14:17)]) %>%
+  cbind(hip_w_IC[c(1:4, 6:9, 14:17)]) %>%
+  cbind(hip_ID[c(5:8, 10:13, 17:20)]) %>%
+  cbind(hip_w_ID[c(5:8, 10:13, 17:20)]) %>%
   cbind(data_DMm) %>%
   cbind(data_JH) %>%
   cbind(data_TS_WM) %>%
@@ -34,23 +36,62 @@ hip_analysis <- hip %>%
 
 # Pooled ------------------------------------------------------------------
 
-## No interaction ---------------------------------------------------------
-
 hip_analysis_pool <- hip_analysis %>%
   mutate(treat2 = ifelse(treat2a == 1 | treat2b == 1, 1, 0),
          .after = treat2b)
 
-# hip_analysis_pool %>% group_by(treat1, treat2) %>% count()
-# there are 123 indivs. who got treat1 and treat2, do we exclude them?
+## No interaction ---------------------------------------------------------
+
+
 
 ## Interaction ------------------------------------------------------------
 
-hip_analysis_pool_int <- hip_analysis %>%
-  mutate(treat2 = ifelse(treat2a == 1 | treat2b == 1, 1, 0),
-         .after = treat2b)
+ggplot(data = hip_analysis_pool) +
+  geom_density(aes(x = guess_entry_salary),
+               color = 'blue',
+               alpha = 0.5,
+               fill = 'blue') +
+  geom_density(aes(x = w_guess_entry_salary),
+               color = 'red',
+               alpha = 0.5,
+               fill = 'red') +
+  geom_vline(aes(xintercept = 750)) +
+  facet_grid(treat1 ~ treat2)
 
-# identical to hip_analysis_pool
 
+ggplot(data = hip_analysis_pool) +
+  geom_density(aes(x = guess_salary_medium),
+               color = 'blue',
+               alpha = 0.5) +
+  geom_density(aes(x = w_guess_salary_medium),
+               color = 'red',
+               alpha = 0.5) +
+  geom_vline(aes(xintercept = 1707)) +
+  facet_grid(treat1 ~ treat2)
+
+ggplot(data = hip_analysis_pool) +
+  geom_density(aes(x = guess_entry_salary_abs),
+               color = 'blue',
+               alpha = 0.5) +
+  geom_density(aes(x = w_guess_entry_salary_abs),
+               color = 'red',
+               alpha = 0.5) +
+#  geom_vline(aes(xintercept = 750)) +
+  facet_grid(treat1 ~ treat2)
+
+hip_analysis_pool <- hip_analysis_pool %>%
+  mutate(guess_entry_salary_diff = w_guess_entry_salary - guess_entry_salary,
+         .after = guess_entry_salary_bias) %>%
+  mutate(guess_salary_medium_diff = w_guess_salary_medium - guess_salary_medium,
+         .after = guess_salary_medium_bias)
+
+reg1 <- lm(guess_entry_salary_diff ~ treat1 + treat2, data = hip_analysis_pool)
+
+reg2 <- lm(guess_salary_medium_diff ~ treat1 + treat2, data = hip_analysis_pool)
+
+stargazer(reg1, type = 'text')
+
+stargazer(reg2, type = 'text')
 
 # Separated ---------------------------------------------------------------
 
