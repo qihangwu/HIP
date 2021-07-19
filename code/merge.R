1b8de96d4eb451acdb870f7fe904efaec43a7e58
Max Zhang <myzhang01@berkeley.edu>
2021-07-18 20:23:18 -0700

    reorganize

--- code/merge.R
+++ code/merge.R
@@ -18,11 +18,20 @@ source('code/Grading_BT_CT_DX.R')
 # we can make the merge process more robust
 
 hip_analysis <- hip %>%
-  select(c('wid', 'today_day', 'firm', 'treat1', 'treat2a', 'treat2b')) %>%
-  cbind(hip_IC[c(1:4, 6:9, 14:17)]) %>%
+  # select the individual-specific variables to group by later on
+  select(c('wid',
+           'today_month',
+           'today_day',
+           'firm',
+           'treat1',
+           'treat2a',
+           'treat2b')) %>%
+
+  cbind(hip_IC[c(1:4, 6:9, 14:17)]) %>%   # selecting only salary variables
   cbind(hip_w_IC[c(1:4, 6:9, 14:17)]) %>%
   cbind(hip_ID[c(5:8, 10:13, 17:20)]) %>%
   cbind(hip_w_ID[c(5:8, 10:13, 17:20)]) %>%
+
   cbind(data_DMm) %>%
   cbind(data_DMe) %>%
   cbind(data_WF) %>%
@@ -34,6 +43,12 @@ hip_analysis <- hip %>%
   cbind(data_CT) %>%
   cbind(data_DX)
 
-# write.csv(hip_analysis, 'data/analysis.csv')
+
+# Export ------------------------------------------------------------------
+
+#write.csv(hip_analysis, 'data/analysis.csv')
+
+
+
 
 
